d10c4896c6ab7e588b1fb5c12226a3344caa9c33
홍태호 <malamute10@xinapse.ai>
2024-12-04 12:46:33 +0900

    chore : 도전과제 ERD 및 SQL 추가

--- schedule.sql
+++ schedule.sql
@@ -1,3 +1,5 @@
+
+## 필수 과제 SQL
 CREATE TABLE schedule (
 
     id                  INT UNSIGNED AUTO_INCREMENT             NOT NULL,
@@ -9,4 +11,31 @@ CREATE TABLE schedule (
     updated_datetime    DATETIME DEFAULT CURRENT_TIMESTAMP()    NOT NULL ON UPDATE CURRENT_TIMESTAMP(),
 
     PRIMARY KEY (id)
+);
+
+
+## 도전 과제 SQL
+CREATE TABLE user (
+
+    id                  INT UNSIGNED    AUTO_INCREMENT      NOT NULL,
+    name                VARCHAR(64)                         NOT NULL,
+    email               VARCHAR(64)                         NOT NULL,
+    created_datetime    DATETIME DEFAULT CURRENT_TIMESTAMP()    NOT NULL,
+    updated_datetime    DATETIME DEFAULT CURRENT_TIMESTAMP()    NOT NULL ON UPDATE CURRENT_TIMESTAMP(),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE schedule (
+
+    id                  INT UNSIGNED AUTO_INCREMENT             NOT NULL,
+    user_id             INT UNSIGNED                            NOT NULL,
+    password            VARCHAR(64)                             NOT NULL,
+    content             TEXT                                    NOT NULL,
+
+    created_datetime    DATETIME DEFAULT CURRENT_TIMESTAMP()    NOT NULL,
+    updated_datetime    DATETIME DEFAULT CURRENT_TIMESTAMP()    NOT NULL ON UPDATE CURRENT_TIMESTAMP(),
+
+    PRIMARY KEY (id),
+    CONSTRAINT fk__schedule__user_id FOREIGN KEY (user_id) REFERENCES user(id)
 );
