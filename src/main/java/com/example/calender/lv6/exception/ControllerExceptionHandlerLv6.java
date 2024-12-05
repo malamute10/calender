ee603b504e67ddbd25760d666505ecf0a7a0dc71
홍태호 <malamute10@xinapse.ai>
2024-12-05 15:30:56 +0900

    feat : Lv6 MethodArgumentNotValidException Handler 추가

--- src/main/java/com/example/calender/lv6/exception/ControllerExceptionHandlerLv6.java
+++ src/main/java/com/example/calender/lv6/exception/ControllerExceptionHandlerLv6.java
@@ -1,7 +1,11 @@
 package com.example.calender.lv6.exception;
 
+import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +20,21 @@ public class ControllerExceptionHandlerLv6 {
         return new ResponseEntity<>(
                 new ErrorResponseLv6(status.value(), e.getMessage()),
                 status
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseLv6> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        HttpStatusCode statusCode = e.getStatusCode();
+
+        List<String> errorMessages = e.getFieldErrors().stream()
+                .map(error -> error.getField() + " - " + error.getDefaultMessage())
+                .toList();
+
+        return new ResponseEntity<>(
+                new ErrorResponseLv6(statusCode.value(), String.join(", ", errorMessages)),
+                statusCode
         );
     }
 }
